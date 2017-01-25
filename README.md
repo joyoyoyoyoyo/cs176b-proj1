f12631dc83da4398a7f83fb6579ff5ec65e3cf79
Angel Ortega <an22ortega@gmail.com>
2017-01-24 19:31:30 -0800

    AO - specifying some future commands and classes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Todos
+Create a server with -l command
+
+Resolve dns and decimal notation
+hostname used by client
+hostname that server should listen for
+
+Two threads
---
+++ main.c
@@ -0,0 +1,13 @@
+//
+// Created by angel on 1/24/17.
+//
+//#include <netinet/in.h>
+#include "sys/socket.h"
+
+int main(int argc, const char* argv[]) {
+
+  int hostname;
+  int port;
+  //struct sockaddr_in socket;
+
+}
