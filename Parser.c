b54d82694cd3b9713671545d3e6533aaa863aa21
Angel Ortega <an22ortega@gmail.com>
2017-01-24 16:50:05 -0800

    AO - init

---
+++ Parser.c
@@ -0,0 +1,5 @@
+//
+// Created by angel on 1/23/17.
+//
+
+struct [] port {}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(String[] args) {
+  command = command.read(args);
+
+  // Netcat();
+  
+
+  cout << "Hello, World!" << endl;
+  command.read();
+
+  return 0;
+}
