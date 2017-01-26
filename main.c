e2d876ce80b2ae60f16a5afd389a237635363149
Angel Ortega <angelortega@umail.ucsb.edu>
2017-01-26 13:22:38 -0800

    AO - parsing the arguments for the C compiler and proj1

--- main.c
+++ main.c
@@ -3,46 +3,54 @@
 //
 //#include <netinet/in.h>
 //#include "sys/socket.h" // depedency for
-#include "unistd.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 struct argument {
-  const char* name;
+    const char* name;
 };
 
 // class of arguments
 struct optional_argument {
-  const char* hostname;
+    const char* hostname;
 }; 
 
 struct snc {
-  const unsigned int port; // required (must be last argument)
-  const int udp_flag;
-  const int listen_flag;
-  //const boolean udp_flag;
-  //const boolean listen_flag;
-  const char* source_ip_address;
-  const char* hostname;
-  //const execute(); //?
-  //const snc();//?
+    unsigned port; // required (must be last argument)
+    int udp_flag;
+    int listen_flag;
+    //const boolean udp_flag;
+    //const boolean listen_flag;
+    char* source_ip_address;
+    char* hostname;
+    //const execute(); //?
+    //const snc();//?
 };
 
-int main(int argc, char**
- argv) {
-  int opt;
+int main(int argc, char**argv) {
+    int opt;
+    char listen_arg;
+    while ( (opt = getopt(argc, argv, "l:")) != -1 ) {
+        switch (opt) {
+            case 'l':
+                listen_arg = opt;
+                struct snc command;
+                command.listen_flag = 'l';
+                listen_flag = 'l';
+                break;
+            case 'u':
 
-  while ( (opt = getopt(argc, argv, "l:")) != -1 ) {
-    switch (opt) {
-    case 'l':
-      break;
-      // Expect A
-      // Expect B
-      // Expect C
-      //default: // parse port
+            default:
+                break;
+            // Expect A
+            // Expect B
+            // Expect C
+            //default: // parse port
+        }
     }
-  }
 
-  //int hostname;
-  //int port;
-  //struct sockaddr_in socket;
+    //int hostname;
+    //int port;
+    //struct sockaddr_in socket;
 
-}
+    }
