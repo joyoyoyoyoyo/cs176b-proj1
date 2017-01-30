f47413a27641cb9cda4cbea4db6a72b64a815918
Angel Ortega <an22ortega@gmail.com>
2017-01-29 23:49:56 -0800

    AO - finalize

--- main.c
+++ main.c
@@ -74,6 +74,7 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  command.hostname = argv[argc-1-num_of_optparams_specified];
 
   // [hostname] argument is required if the [-l] flag is not given
   if (command.listen_flag == NULL && command.hostname == NULL ) {
@@ -112,27 +113,65 @@ int main(int argc, char* argv[]) {
   for(index =0; index < argc; index++)
     printf("argv[%d]:\t%s\n",index, argv[index]);
 
+  // Construct either a UDP or TCP socket
+  int byte_type;
+  if (command.udp_flag != 0)
+    byte_type = SOCK_DGRAM;
+  else
+    byte_type = SOCK_STREAM;
+  int socketfd = socket(AF_INET, byte_type, 0);
+
+  // Create a send and recv buffer
+  char buffer[DEFAULT_BUFFER_SIZE]; // send buffer
+  char *buffer_addr;
+  ssize_t n_bytes_to_read;
+  int buffer_free_space;
+  buffer_addr = buffer;
+  buffer_free_space = DEFAULT_BUFFER_SIZE;
+
+  // receive buffer
+  char recv_buffer[DEFAULT_BUFFER_SIZE];
+  char *recv_buffer_addr;
+  ssize_t n_bytes_to_read_recvB;
+  int recv_buffer_free_space;
+  buffer_addr = buffer;
+  buffer_free_space = DEFAULT_BUFFER_SIZE;
+
   // MUX logic for specifying what the behavior should be for the commands
   //TODO: MUX
-
-  // construct udp client on the valid port to the destination address
   struct sockaddr_in *client_addr;
+  // construct udp client on the valid port to the destination address
   if (command.udp_flag != 0 && command.hostname != 0 && command.port != 0) {
     mux.controller = 1;
+    connect( socketfd, ( struct sockaddr * ) &client_addr, sizeof( client_addr ) );
 
-    // configure default IPv4
-    client_addr->sin_family = AF_INET; // use IPv4
 
-    // construction destination address for the datagram
-    // Specifically the port and [hostname]
-    client_addr->sin_port = htons((uint16_t ) command.port); // set port
-    struct hostent *host_information;
-    host_information = gethostbyname(command.hostname);
-    if (host_information == NULL) {
-      printf(ERROR_INTERNAL_MESSAGE);
-      exit (0);
-    }
-    client_addr->sin_addr = *(struct in_addr *) host_information->h_addr;
+    // send message
+    char* message = "Test message";
+    send( socketfd, message, strlen(message), 0);
+
+//    send( socketfd, buffer_addr, strlen(buffer_addr), 0);
+
+    bind(socketfd, (struct sockaddr *)client_addr, sizeof(client_addr));
+
+    listen( socketfd, 1000 );
+
+    // receive response
+    recv( socketfd, recv_buffer,sizeof(recv_buffer),DEFAULT_BUFFER_SIZE );
+
+//    // configure default IPv4
+//    client_addr->sin_family = AF_INET; // use IPv4
+//
+//    // construction destination address for the datagram
+//    // Specifically the port and [hostname]
+//    client_addr->sin_port = htons((uint16_t ) command.port); // set port
+//    struct hostent *host_information;
+//    host_information = gethostbyname(command.hostname);
+//    if (host_information == NULL) {
+//      printf(ERROR_INTERNAL_MESSAGE);
+//      exit (0);
+//    }
+//    client_addr->sin_addr = *(struct in_addr *) host_information->h_addr;
 
 
   }
@@ -150,28 +189,18 @@ int main(int argc, char* argv[]) {
 //  client_addr->sin_addr.s_addr = inet_addr(command.hostname);
   //  inet_aton("63.161.169.137", &myaddr.sin_addr.s_addr);
 
-  int byte_type;
-  if (command.udp_flag != 0)
-    byte_type = SOCK_DGRAM;
-  else
-    byte_type = SOCK_STREAM;
-
-  int socketfd = socket(AF_INET, byte_type, 0);
-  bind(socketfd, (struct sockaddr *)client_addr, sizeof(client_addr));
-
-  listen( socketfd, 1000 );
 
 
   /***
    * Construct buffer for server
    */
   char *req = "Test";
-  char buffer[DEFAULT_BUFFER_SIZE];
-  char *buffer_addr;
-  ssize_t n_bytes_to_read;
-  int buffer_free_space;
-  buffer_addr = buffer;
-  buffer_free_space = DEFAULT_BUFFER_SIZE;
+//  char buffer[DEFAULT_BUFFER_SIZE];
+//  char *buffer_addr;
+//  ssize_t n_bytes_to_read;
+//  int buffer_free_space;
+//  buffer_addr = buffer;
+//  buffer_free_space = DEFAULT_BUFFER_SIZE;
 
   int conn_fd_passthrough = accept(socketfd, ( struct sockaddr * ) &client_addr, (socklen_t *) &client_addr);
    while ( (n_bytes_to_read = recv( conn_fd_passthrough, buffer_addr, DEFAULT_BUFFER_SIZE, 0)) > 0) {
@@ -179,37 +208,7 @@ int main(int argc, char* argv[]) {
      buffer_free_space -= n_bytes_to_read;
    }
   send( conn_fd_passthrough, buffer_addr, strlen(req), 0);
-//  struct snc commands = parseCommands(argc,argv);
-//  deploy(*commands,commands->port,commands->udp_flag);
-
 
 }
 
-//void deploy(struct snc simple_nc, unsigned port,int udp_flag) {
-//
-//
-//
 ////  connect(socketfd, ,)
-//
-//
-//  /**
-//   * Base case: -l snc command (UDP server)
-//   */
-//  createUDPClient();
-//}
-
-/**
- * The assumption is that we are working with the [-u], [-l], [port number]
- */
-void createUDPClient() {
-  //
-}
-
-void parseCommands(struct snc command, int argc, char *argv[]) {
-
-
-}
-
-/**
- * If -l
- */
